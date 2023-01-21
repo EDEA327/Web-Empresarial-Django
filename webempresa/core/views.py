777a7faeb3fdb18e3bd6cc595ecca36349c46f29
Erick <edeajob27@gmail.com>
2023-01-21 10:19:19 -0500

    creación y preparación de la app core

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -4,3 +4,25 @@ from django.shortcuts import render,HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
+
+def services(request):
+    return HttpResponse('<h1>Servicios</h1>')
+
+def store(request):
+    return HttpResponse('<h1>visitanos</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contacto</h1>')
+
+def blog(request):
+    return HttpResponse('<h1>Blog</h1>')
+
+def sample(request):
+    return HttpResponse('<h1>Sample</h1>')
+
+
+
+
