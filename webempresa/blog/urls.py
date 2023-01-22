1519556309453de75f4fa58942e505aa0d54fd52
Erick <edeajob27@gmail.com>
2023-01-22 11:46:45 -0500

    creando la vista de category para filtrar por cateogrias

--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.blog,name='blog'),
+    #! Para que la url tome un elemento...
+    #! dinamico se debe pasar entre <> y si se..
+    #! Quiere caster se pone type_cast:parametro
+    #* Siempre se pasa una string por lo mismo se debe castear
+    path('category/<int:category_id>/',views.category,name='category'),
 ]
--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Category
 # Create your views here.
+
+def category(request,category_id):
+    category = Category.objects.get(id=category_id)
+    context = {
+        'category': category
+    }
+    return render(request, 'blog/category.html', context)
 
 def blog(request):
     posts = Post.objects.all()
