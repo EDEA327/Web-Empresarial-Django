3d1c81b286885bc86a7033c5e49f3835ccd7ffab
Erick <edeajob27@gmail.com>
2023-01-24 17:31:44 -0500

    Final Preparación app Pages

--- webempresa/pages/admin.py
+++ webempresa/pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Page
 # Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Page, PageAdmin)
--- webempresa/pages/models.py
+++ webempresa/pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creacion')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='Fecha de edicion')
+
+    class Meta:
+        verbose_name = 'Página'
+        verbose_name_plural = 'Páginas'
+        ordering = ['title']
+
+    def __str__(self) -> str:
+        return self.title
